533152f29cf479e3758e7365b509d435a96c16f0
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-08-18 05:22:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+const newHeader=document.querySelector('h1');
+newHeader.id = "victory";
+newHeader.textContent = "Wilson  is the champion";
